9e1a842743e75bb8e5e8c015efdb485182db7b13
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-06-11 22:37:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,8 +8,8 @@ async def start(client, message):
         [InlineKeyboardButton("Developer🧑‍💻", url="https://t.me/naviya2"),
         InlineKeyboardButton("Rate us ★", url="https://t.me/tlgrmcbot?start=leoyoutubedownloaderbot-review")],
         
-        [InlineKeyboardButton("Updates Channel🗣💻", url="https://t.me/new_ehi"),
-        InlineKeyboardButton("Support Group👥💻", url="https://t.me/leosupportx")]
+        [InlineKeyboardButton("Updates Channel 🗣", url="https://t.me/new_ehi"),
+        InlineKeyboardButton("Support Group 👥", url="https://t.me/leosupportx")]
     ])
     welcomed = f"Hello <b>{message.from_user.first_name}👋\n\nYou are Warmly welcome to Leo YT Downloader Bot 🇱🇰</b>\n\nIf you want to know how i works just touch on /help command 🙂"
     await message.reply_text(welcomed, reply_markup=joinButton)
