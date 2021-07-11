da4a695209629deb6f8d737886b7128f3975788b
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 01:07:21 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -2,8 +2,6 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 from helper.forcesub import ForceSub
 import asyncio
 import config
-from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 
 @Client.on_message(Filters.command(["help"]))
