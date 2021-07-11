aebbab155aaf793540cdb70296155e6216f63c28
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 00:06:40 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,8 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
+    FSub = await ForceSub(bot, event)
+    if FSub == 400:
+        return 
     helptxt = f"<b>Currently Only Support single url(Don't Send Playlists)😶\n\n1.Just Send Youtube Url in to this Bot 🙂\n\n2.Select The Quality and Format🙂\n\n3.Then select Type of Your Out put🙂</b>"
     await message.reply_text(helptxt)
