6f95d04971dfa08af1931e4f826de60ec4acd2f5
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 17:10:03 +0530

    Update user_stats.py

--- plugins/user_stats.py
+++ plugins/user_stats.py
@@ -1,7 +1,21 @@
 #Naviya2
 
+import os
+import time
+import psutil
+import shutil
+import string
+import asyncio
+import config
+from helper.broadcast import broadcast_handler
+from helper.database.add_user import AddUserToDatabase
+from helper.display_progress import humanbytes
+from pyrogram import Client, filters, StopPropagation
+from helper.forcesub import ForceSub
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-@app.on_message(filters.private & filters.command("stats") & filters.user(config.BOT_OWNER))
+@Client.on_message(filters.private & filters.command("stats") & filters.user(config.BOT_OWNER))
 async def show_status_count(_, client: Message):
     total, used, free = shutil.disk_usage(".")
     total = humanbytes(total)
