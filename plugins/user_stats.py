ed8574b881493296b98cd1d5f513da4cd814f01d
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 17:06:43 +0530

    Create user_stats.py

---
+++ plugins/user_stats.py
@@ -0,0 +1,18 @@
+#Naviya2
+
+
+@app.on_message(filters.private & filters.command("stats") & filters.user(config.BOT_OWNER))
+async def show_status_count(_, client: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count()
+    await client.reply_text(
+        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`\n\n@leosongdownloaderbot 🇱🇰",
+        parse_mode="Markdown",
+        quote=True
+    )
