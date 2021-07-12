8d752abf8a7922d4ea1e8b3e13deb7ed3290f59b
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 09:41:45 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -8,7 +8,7 @@ import config
 
 @Client.on_message(filters.command(["help"]))
 async def start(client, message):
-    FSub = await ForceSub(bot, event)
+    FSub = await ForceSub(client, message)
     if FSub == 400:
         return 
     helptxt = f"<b>Currently Only Support single url(Don't Send Playlists)😶\n\n1.Just Send Youtube Url in to this Bot 🙂\n\n2.Select The Quality and Format🙂\n\n3.Then select Type of Your Out put🙂</b>"
