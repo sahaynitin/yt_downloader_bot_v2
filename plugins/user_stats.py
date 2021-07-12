04f61ed9e15960c78e4209c2f59c431d5c14aea2
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 17:38:28 +0530

    Update user_stats.py

--- plugins/user_stats.py
+++ plugins/user_stats.py
@@ -26,7 +26,7 @@ async def show_status_count(_, client: Message):
     disk_usage = psutil.disk_usage('/').percent
     total_users = await db.total_users_count()
     await client.reply_text(
-        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`\n\n@leosongdownloaderbot 🇱🇰",
+        text=f"**Total Disk Space:** {total} \n**Used Space:** {used}({disk_usage}%) \n**Free Space:** {free} \n**CPU Usage:** {cpu_usage}% \n**RAM Usage:** {ram_usage}%\n\n**Total Users in DB:** `{total_users}`\n\n@leoyoutubedownloaderbot 🇱🇰",
         parse_mode="Markdown",
         quote=True
     )
