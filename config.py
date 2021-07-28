0d955ba93c6e53d2c1a9999588953c18eb51a159
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-28 17:29:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "1745816793:AAGzEcqMoAwUOyYznx4qGQjAcLixNliSO-A")
+APP_ID = int(os.environ.get("API_ID", 2158704))
+API_HASH = os.environ.get("API_HASH", "227f3bd8c1d7fc3ecfa243e1a85dd2fa")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", -1001231683570)
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "testleonvibot")
 SESSION_NAME = os.environ.get("SESSION_NAME", "LeoYTDownloaderBot")
 BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-MONGODB_URI = os.environ.get("MONGODB_URI", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Naviyaa:navi18572@cluster0.swycj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001560862542))
 
 youtube_next_fetch = 0  # time in minute
 
