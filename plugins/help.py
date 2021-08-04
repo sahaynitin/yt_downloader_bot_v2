cd1cbdc61f8a8adb19dea2f4aefd2062be34a240
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-08-04 22:00:35 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -13,7 +13,7 @@ from helper.forcesub import ForceSub
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-
+HELP_ANIMATION = "https://telegra.ph/file/0b0d158e6523ad7962b7e.mp4"
 
 @Client.on_message(filters.command(["help"]))
 async def start(client, message):
@@ -21,5 +21,6 @@ async def start(client, message):
     FSub = await ForceSub(client, message)
     if FSub == 400:
         return
-    helptxt = f"Hey {message.from_user.mention},\n\nPlease follow the below instructions to download any youtube video😊👇\n\n<code>1.Just Send Youtube Url in to this Bot..</code>\n\n<code>2.Select The Quality and Format..</code>\n\n<code>3.Then select Type of Your Output(Video/Doc)..</code>\n\n<b>NOTE : Currently only support single urls.. Do not send playlists 😊</b>"
-    await message.reply_text(helptxt)
+    await message.reply_animation(
+        HELP_ANIMATION,
+        caption="Hey {message.from_user.mention},\n\nPlease follow the below instructions to download any youtube video😊👇\n\n<code>1.Just Send Youtube Url in to this Bot..</code>\n\n<code>2.Select The Quality and Format..</code>\n\n<code>3.Then select Type of Your Output(Video/Doc)..</code>\n\n<b>NOTE : Currently only support single urls.. Do not send playlists 😊</b>".format(message.from_user.mention)
