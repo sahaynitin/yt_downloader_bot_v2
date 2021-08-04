e1c20d7089fb54ec67e2ea42f236494809c6719e
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-08-05 00:49:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", -1001231683570)
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "testleonvibot")
 SESSION_NAME = os.environ.get("SESSION_NAME", "LeoYTDownloaderBot")
 BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://Naviyaa:navi18572@cluster0.swycj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001560862542))
 
