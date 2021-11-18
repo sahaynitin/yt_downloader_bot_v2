3f6986d49a28fab6660771487afa33e492f52cff
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-11-19 00:35:14 +0530

    test

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,21 +1,13 @@
 #@Naviya2
 
-import os
-import time
-import psutil
-import shutil
-import string
-import asyncio
 import config
 from helper.broadcast import broadcast_handler
 from helper.database.add_user import AddUserToDatabase
 from helper.display_progress import humanbytes
-from pyrogram import Client, filters, StopPropagation
-from helper.forcesub import ForceSub
-from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
 
 @Client.on_message(filters.private & filters.command("broadcast") & filters.user(config.BOT_OWNER) & filters.reply)
-async def _broadcast(_, client: Message):
-    await broadcast_handler(client)
+async def broadcast(_, m: Message):
+    await broadcast_handler(m)
